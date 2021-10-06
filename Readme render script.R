e57775fdda94146a6a6fd104bddd0552e61c2e51
dberger2 <dberger2@ncsu.edu>
2021-10-05 22:01:55 -0400

    pushing updated render script

--- Readme render script.R
+++ Readme render script.R
@@ -3,17 +3,5 @@ render(
   "project1.Rmd",
   output_format = "github_document" (html_preview = FALSE),
   output_file = "README.md",
-  output_dir = NULL,
-  output_options = NULL,
-  output_yaml = NULL,
-  intermediates_dir = NULL,
-  knit_root_dir = NULL,
-  runtime = c("auto", "static", "shiny", "shinyrmd", "shiny_prerendered"),
-  clean = TRUE,
-  params = NULL,
-  knit_meta = NULL,
-  envir = parent.frame(),
-  run_pandoc = TRUE,
-  quiet = FALSE,
-  encoding = "UTF-8"
+    encoding = "UTF-8"
 )
